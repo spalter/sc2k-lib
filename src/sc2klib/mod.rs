2309b14ff826b514f7d9ad45e5850f0f31f7ba60
Spalt(er) <mail@spalt3r.de>
2024-05-27 20:33:39 +0200

    Fixed lib example code to make it compile.

--- src/sc2klib/mod.rs
+++ src/sc2klib/mod.rs
@@ -1,11 +1,13 @@
 //! SimCity 2000 save file library.
-//! 
+//!
 //! Provides an interface to read SimCity 2000 save files.
 //! With additional json output and debug information.
-//! 
-//! # Example
-//! ```rust
-//! let city_data = SC2KFile::new("./sc2kcity.sc2")?;
+//!
+//! # Examples
+//! ```
+//! use sc2klib::sc2kfile::SC2KFile;
+//!
+//! let city_data = SC2KFile::new(String::from("assets/Utopia.sc2")).unwrap();
 //! println!("{:?}", city_data.to_json());
 //! ```
 
