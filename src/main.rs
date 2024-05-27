b354225fa371430ca3e9337bb211624c1a4c505b
Spalt(er) <mail@spalt3r.de>
2024-05-27 19:22:37 +0200

    Parameter clean up

--- src/main.rs
+++ src/main.rs
@@ -17,19 +17,17 @@ fn main() -> std::io::Result<()> {
         .nth(1)
         .expect("Missing action e.g. --json or --debug.");
 
+    let path = std::env::args()
+        .nth(2)
+        .expect("Missing file path eg. cities/my_city.sc2.");
+
     match pattern.as_str() {
         "-d" | "--debug" => {
-            let path = std::env::args()
-                .nth(2)
-                .expect("Missing file path eg. cities/my_city.sc2.");
             println!("Try load: {:?}", &path);
             let city_data = SC2KFile::new(path)?;
             println!("{:?}", city_data.map.stats);
         }
         "-j" | "--json" => {
-            let path = std::env::args()
-                .nth(2)
-                .expect("Missing file path eg. cities/my_city.sc2.");
             let city_data = SC2KFile::new(path)?;
             println!("{}", city_data.to_json());
         }
